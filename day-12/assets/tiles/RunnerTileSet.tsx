dd68df279ff24cdaa9eeee0ffc8735f5d3516098
owaaquino <josejoshuaaquino@gmail.com>
2026-02-19 17:04:12 +0800

    Day 12 basic tileset

---
+++ day-12/assets/tiles/RunnerTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="RunnerTileSet" tilewidth="32" tileheight="32" tilecount="78" columns="13">
+ <image source="../../../../../../../Downloads/Space Runner Assets - 27-11-20/Space Runner Assets - 27-11-20/Tiles/RunnerTileSet.png" width="432" height="192"/>
+</tileset>
