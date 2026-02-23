a1122f313e01d26c5b5e44ba12823262977545df
owaaquino <josejoshuaaquino@gmail.com>
2026-02-23 16:01:44 +0800

    Day 12 background and objects

--- day-12/assets/tiles/platform.tsx
+++ day-12/assets/tiles/platform.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="platform" tilewidth="32" tileheight="32" tilecount="12" columns="12">
- <image source="../../day-11/assets/platform.png" width="400" height="32"/>
+ <image source="../platform.png" width="400" height="32"/>
 </tileset>
